0e1455f1ade1294b0283adc4b5377e9327ece1b9
Jonh Doe <JonhDoe@gmail.com>
2022-08-15 21:25:05 -0300

    Atualização, sistema finalizado.

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 console.log("Sistema de contagem e classificação: ");
 
 const prompt = require("prompt-sync")();
+const fs = require('fs');
 
-var quantidade = prompt("Qual será a quantidade de números? ");
+var quantidade = 0;
+var nomes = [];
+var dat = [];
 
-for(x = 0; x <= quantidade; x++){
-if(x == 0){
-    console.log(x + " Zero localizado!");
+pergunta();
+
+function pergunta(){
+nomes[quantidade] = prompt("Qual o nome do aluno?")
+quantidade++;
+console.log(quantidade);
+perg = prompt("Deseja adicionar mais um aluno? (s ou n)");
+
+if(perg == "s" || perg == "S"){
+pergunta();
 }
-else if(x % 2 == 0){
-    console.log(x + " Número par encontrado!");
+
+else if(perg == "n" || perg == "N"){
+    console.log("-------------------------");
+    console.log("Relatório:");
+    console.log("-------------------------");
+    for(x = 0; x < quantidade; x++){
+        if(x == 0){
+            console.log("" + nomes[x] + ", Seção: " + x + ", Zero localizado!");
+            dat = dat + "" + nomes[x] + ", Seção: " + x + ", Zero localizado!\n";
+        }
+        else if(x % 2 == 0){
+            console.log("" + nomes[x] + ", Seção: " + x + ", Número par encontrado!");
+            dat = dat + "" + nomes[x] + ", Seção: " + x + ", Número par encontrado!\n";
+        }
+        else{
+            console.log("" + nomes[x] + ", Seção: " + x + ", Número ímpar encontrado!");
+            dat = dat + "" + nomes[x] + ", Seção: " + x + ", Número ímpar encontrado!\n";
+        }
+        };
+    console.log("-------------------------");
 }
-else{
-    console.log(x + " Número ímpar encontrado!");
+projeto = prompt("Qual será o nome do relatório?");
+log();
 }
-};
+
+function log(){
+      
+    const data = "-------------------------\n" + "Relatório:\n" + "-------------------------\n" + dat + "-------------------------";
+    fs.writeFileSync(projeto + ".txt", data);
+    console.log("Relatório adicionado à pasta do sistema!");
+      
+    prompt("Sistema finalizado!");
+    }
+
